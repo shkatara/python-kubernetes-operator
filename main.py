c6a624815058f717a62b115ac8a49ead44446832
Shubham Katara <shubham.katara@trivago.com>
2024-02-21 15:02:49 +0100

    Delete configmap for each pod when pod is created

--- main.py
+++ main.py
@@ -11,11 +11,20 @@ podInfo = CoreV1Api.list_namespaced_pod(namespace="default")
 #    print(pod)
 
 def createConfigmap(pod_name: str):
-    cmap = client.V1ConfigMap()
-    cmap.data = {}
-    cmap.metadata = client.V1ObjectMeta(name=f'pod-{pod_name}')
-    cmap.data["Pod_Name"] = pod_name
-    CoreV1Api.create_namespaced_config_map(namespace="default", body=cmap)
+    try:
+        cmap = client.V1ConfigMap()
+        cmap.data = {}
+        cmap.metadata = client.V1ObjectMeta(name=f'pod-{pod_name}')
+        cmap.data["Pod_Name"] = pod_name
+        #print(CoreV1Api.read_namespaced_config_map(name=f'pod-{pod_name}'))
+        CoreV1Api.create_namespaced_config_map(namespace="default", body=cmap)
+    except:
+        return {"Error:" "Erorr creating configmap"}
+   
+
+def deleteConfigMap(cm: str):
+    print(cm)
+    CoreV1Api.delete_namespaced_config_map(name=cm,namespace="default")
 
 #Capture Events of pod creation in default namespace. 
 for event in watch.stream(func=CoreV1Api.list_namespaced_pod,namespace="default"):
@@ -26,3 +35,4 @@ for event in watch.stream(func=CoreV1Api.list_namespaced_pod,namespace="default"
     if event['type'] == "DELETED":
         pod_name = event['object'].metadata.name
         print(f'Pod with name {pod_name} Deleted')
+        deleteConfigMap(f'pod-{pod_name}')
